9bfe597ff5a9ab44ebec4d25baeb8a3c1f9a9c78
Dwayne <saulsdwayne@gmail.com>
2016-10-03 15:55:13 +0200

    last ones

--- conditional-logic.js
+++ conditional-logic.js
@@ -3,5 +3,7 @@ captureTimeChange(captureTrafficLightChange);
 function captureTrafficLightChange(number){
     //write your code here!
     // number is the value entered in the text on the screen
-
+if (number > 40) {
+  go();
+}
 }
